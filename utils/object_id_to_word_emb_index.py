fcc07e137601f1b906e2a171f802a93e2087cb44
omeir1 <ohad.meir@intel.com>
2018-07-25 18:58:52 +0300

    object_id_to_word_emb_index.py

---
+++ utils/object_id_to_word_emb_index.py
@@ -0,0 +1,32 @@
+import argparse
+
+
+def main(args):
+    content = [l.strip() for l in open(args.words_vocab_path).readlines()]
+    word_to_index = {content[i]: str(i) for i in range(len(content))}
+
+    object_descs = [l.strip().split(',') for l in open(args.objects_desc_path).readlines()]
+
+    with open(args.output_path, 'w') as f:
+
+        for l in object_descs:
+
+            id = l[0]
+            words = l[1].split()
+
+            indexes = []
+            for w in words:
+                if w in word_to_index:
+                    indexes += [word_to_index[w]]
+                else:
+                    print('N/A {}'.format(w))
+
+            f.write('{},{}\n'.format(id, ' '.join(indexes)))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--words_vocab_path', type=str, required=True)
+    parser.add_argument('--objects_desc_path', type=str, required=True)
+    parser.add_argument('--output_path', type=str, required=True)
+    main(parser.parse_args())
