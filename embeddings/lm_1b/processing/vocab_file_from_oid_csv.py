9bdefe44095cd7762fa77f2596e721943a2685af
omeir1 <ohad.meir@intel.com>
2018-07-25 17:58:10 +0300

    oid_vocab.txt - remove wrong object ids

--- embeddings/lm_1b/processing/vocab_file_from_oid_csv.py
+++ embeddings/lm_1b/processing/vocab_file_from_oid_csv.py
@@ -3,14 +3,11 @@ import argparse
 
 def process_vocab(words_set, path):
     content = open(path).readlines()
-    content = [l.strip().split(',') for l in content]
+    content = [l.strip().split(',')[1] for l in content]
     for entry in content:
-        id = entry[0]
-        word = entry[1]
-        words = word.replace('(', '').replace(')', '').split()
+        words = entry.replace('(', '').replace(')', '').split()
         for w in words:
-            if w not in words_set:
-                words_set[w] = id
+            words_set.add(w)
 
 
 def main(args):
@@ -20,17 +17,14 @@ def main(args):
     output_path = args.output_path
 
     vocabs = [classes_vocab, attributes_vocab, relationships_vocab]
-    words_set = {'<S>': '<S>', '</S>': '</S>'}
+    words_set = {'<S>', '</S>'}
 
     for v in vocabs:
         process_vocab(words_set, v)
 
     with open(output_path, 'w') as f:
-        content = open(r'D:\Projects\OpenImagesChallenge\image-caption\embeddings\lm_1b\files\original_words_list_for_embeddings_order.txt').readlines()
-        for l in content:
-            w = l.strip()
-            id = words_set[w]
-            f.write('{},{}\n'.format(w, id))
+        for w in words_set:
+            f.write('{}\n'.format(w))
 
 
 if __name__ == '__main__':
